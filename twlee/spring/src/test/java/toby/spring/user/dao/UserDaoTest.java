2820e3b539fac831c65881f3a1dd288abb62b3c2
doorisopen <doorwasopen@gmail.com>
2022-08-04 21:37:05 +0900

    [twlee] 2022-08-04 1.1 초난감 DAO

---
+++ twlee/spring/src/test/java/toby/spring/user/dao/UserDaoTest.java
@@ -0,0 +1,23 @@
+package toby.spring.user.dao;
+
+import toby.spring.user.domain.User;
+
+import java.sql.SQLException;
+
+class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("gildong1");
+        user.setName("홍길동");
+        user.setPassword("1004");
+
+        userDao.add(user);
+
+        System.out.println("user = " + user + " 등록 성공!");
+
+        User findUser = userDao.get(user.getId());
+        System.out.println(findUser.getName());
+    }
+}
