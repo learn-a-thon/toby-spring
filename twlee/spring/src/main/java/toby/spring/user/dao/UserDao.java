9613a67a768eb1be4cd900a8d333eb9b4d5693fa
doorisopen <doorwasopen@gmail.com>
2022-08-04 23:37:05 +0900

    [twlee] 2022-08-04 1.2.2 getConnection 메소드 추출

--- twlee/spring/src/main/java/toby/spring/user/dao/UserDao.java
+++ twlee/spring/src/main/java/toby/spring/user/dao/UserDao.java
@@ -7,8 +7,7 @@ import java.sql.*;
 public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+        Connection conn = getConnection();
 
         PreparedStatement ps = conn.prepareStatement("insert into users (id, name, password) values (?, ?, ?)");
         ps.setString(1, user.getId());
@@ -22,8 +21,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+        Connection conn = getConnection();
 
         PreparedStatement ps = conn.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
@@ -40,5 +38,10 @@ public class UserDao {
         conn.close();
 
         return user;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
     }
 }
