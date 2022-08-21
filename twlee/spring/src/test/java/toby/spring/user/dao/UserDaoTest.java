c95419a4bff3374a0f35010cd99c660373528f3b
doorisopen <doorwasopen@gmail.com>
2022-08-21 20:36:26 +0900

    [twlee] 2022-08-21 2.2 UserDaoTest 개선

--- twlee/spring/src/test/java/toby/spring/user/dao/UserDaoTest.java
+++ twlee/spring/src/test/java/toby/spring/user/dao/UserDaoTest.java
@@ -1,30 +1,31 @@
 package toby.spring.user.dao;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import toby.spring.user.domain.User;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+    //junit 5 는 메소드에 접근제어자(public)을 생략해도된다.
+    @Test
+    void addAndGet() throws SQLException, ClassNotFoundException {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
-        user.setId("gildong1");
+        user.setId("gildong2");
         user.setName("홍길동");
-        user.setPassword("1004");
+        user.setPassword("9999");
 
         userDao.add(user);
         System.out.println("user = " + user + " 등록 성공!");
 
         User findUser = userDao.get(user.getId());
-        if (!findUser.getName().equals(user.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!findUser.getPassword().equals(user.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
+        assertEquals(user.getName(), findUser.getName());
+        assertEquals(user.getPassword(), findUser.getPassword());
     }
 }
