1f71a6dded84cb57a057664c9915bb160307c602
doorisopen <doorwasopen@gmail.com>
2022-08-21 20:30:26 +0900

    [twlee] 2022-08-21 2.2.1 테스트 검증의 자동화

--- twlee/spring/src/test/java/toby/spring/user/dao/UserDaoTest.java
+++ twlee/spring/src/test/java/toby/spring/user/dao/UserDaoTest.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 
 class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-//        UserDao userDao = new DaoFactory().userDao();
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
@@ -17,10 +16,15 @@ class UserDaoTest {
         user.setPassword("1004");
 
         userDao.add(user);
-
         System.out.println("user = " + user + " 등록 성공!");
 
         User findUser = userDao.get(user.getId());
-        System.out.println(findUser.getName());
+        if (!findUser.getName().equals(user.getName())) {
+            System.out.println("테스트 실패 (name)");
+        } else if (!findUser.getPassword().equals(user.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        } else {
+            System.out.println("조회 테스트 성공");
+        }
     }
 }
