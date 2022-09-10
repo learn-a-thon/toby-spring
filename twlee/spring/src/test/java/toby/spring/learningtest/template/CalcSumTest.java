a4629ae4732d0e39dff3c57e1defce9ac02b1fc9
doorisopen <doorwasopen@gmail.com>
2022-09-10 18:31:04 +0900

    [twlee] 2022-09-10 3.5 템플릿과 콜백

--- twlee/spring/src/test/java/toby/spring/learningtest/template/CalcSumTest.java
+++ twlee/spring/src/test/java/toby/spring/learningtest/template/CalcSumTest.java
@@ -1,5 +1,6 @@
 package toby.spring.learningtest.template;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
@@ -10,27 +11,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalcSumTest {
 
+    private Calculator calculator;
+    private String filePath;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+        filePath = "./src/test/resources/numbers.txt";
+    }
+
     @Test
     void sumOfNumbers() {
-        Calculator calculator = new Calculator();
-        int sum = calculator.sum("./src/test/resources/numbers.txt");
-        assertEquals(sum, 10);
+        int result = calculator.fileReadTemplate(filePath,
+                br -> {
+                    int sum = 0;
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sum += Integer.parseInt(line);
+                    }
+                    return sum;
+                });
+        assertEquals(result, 10);
+    }
+
+    @Test
+    void multiplyOfNumbers() {
+        int result = calculator.fileReadTemplate(filePath,
+                br -> {
+                    int mul = 1;
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        mul *= Integer.parseInt(line);
+                    }
+                    return mul;
+                });
+        assertEquals(result, 24);
+    }
+
+    @Test
+    void sumOfNumbersLineCallback() {
+        int result = calculator.lineReadTemplate(filePath,
+                (line, initVal) -> initVal + Integer.parseInt(line),
+                0);
+        assertEquals(result, 10);
+    }
+
+    @Test
+    void multiplyOfNumbersLineCallback() {
+        int result = calculator.lineReadTemplate(filePath,
+                (line, initVal) -> initVal * Integer.parseInt(line),
+                1);
+        assertEquals(result, 24);
+    }
+
+    @Test
+    void concatenate() {
+        String result = calculator.lineReadTemplate(filePath,
+                (line, initVal) -> initVal + line,
+                "");
+        assertEquals(result, "1234");
     }
 
     private class Calculator {
         public Calculator() {
         }
 
-        public int sum(String filePath) {
+        public <T> T lineReadTemplate(String filePath, LineCallback<T> callback, T initVal) {
             BufferedReader br = null;
-            int sum = 0;
+            T result = initVal;
             try {
                 br = new BufferedReader(new FileReader(filePath));
                 String line = null;
                 while ((line = br.readLine()) != null) {
-                    sum += Integer.parseInt(line);
+                    result = callback.doSomethingWithLine(line, result);
                 }
-                return sum;
+                return result;
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -42,7 +97,26 @@ public class CalcSumTest {
                     e.printStackTrace();
                 }
             }
-            return sum;
+            return result;
+        }
+
+        public int fileReadTemplate(String filePath, BufferedReaderCallback callback) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new FileReader(filePath));
+                return callback.doSomethingWithReader(br);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (br != null) {
+                        br.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return 0;
         }
     }
 }
